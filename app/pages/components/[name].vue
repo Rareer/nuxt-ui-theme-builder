c3802f72400f7291526276b14e5685efa8f119e3
Toni K <privat@tonikritz.de>
2025-08-19 16:54:19 +0200

    fix applying config classes at reload

--- app/pages/components/[name].vue
+++ app/pages/components/[name].vue
@@ -1,6 +1,8 @@
 <template>
 	<h1 class="text-2xl font-bold mb-6">U{{ component.label }}</h1>
-	<DynamicComponent :component="component.value" />
+	<ClientOnly>
+		<DynamicComponent :component="component.value" />
+	</ClientOnly>
 </template>
 
 <script setup lang="ts">
